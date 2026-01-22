f552b636101e593d327132cc19d51178f2315264
Meo-ICAR <piergiuseppe.meo@icar.cnr.it>
2026-01-22 18:52:59 +0100

    m

--- database/migrations/2026_01_18_111156_create_mandanti_table.php
+++ database/migrations/2026_01_18_111156_create_mandanti_table.php
@@ -24,7 +24,6 @@ return new class extends Migration {
             $table->string('stripe_subscription_id')->nullable();
             $table->enum('periodicita', [1, 2, 3, 6])->default(2)->comment('1=Mensile, 2=Bimestrale, 3=Trimestrale, 6=Semestrale');
             $table->timestamp('stripe_subscription_ends_at')->nullable();
-            $table->boolean('is_active')->default(true);
 
             $table
                 ->foreignUlid('holding_id')
